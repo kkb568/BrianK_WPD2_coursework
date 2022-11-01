351a8f2863b3cf7bf1cccf8294a503d1b2794bf3
kkb568 <b.koome@alustudent.com>
2022-11-01 12:53:11 +0400

    fifth commit

--- pages/js/Modal.js
+++ pages/js/Modal.js
@@ -13,3 +13,11 @@ function openModal1() {
 function closeModal1() {
     document.getElementById("Modal1").style.display = "none";
 }
+
+function openModal2() {
+    document.getElementById("Modal2").style.display = "block";
+}
+
+function closeModal2() {
+    document.getElementById("Modal2").style.display = "none";
+}
