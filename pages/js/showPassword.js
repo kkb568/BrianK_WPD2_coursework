e813a8d0573267cc6fd180471a82dbd195382572
kkb568 <b.koome@alustudent.com>
2022-11-18 23:03:16 +0400

    35th commit

---
+++ pages/js/showPassword.js
@@ -0,0 +1,8 @@
+function showPassword(input) {
+    var x = document.getElementById(input);
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+} 
